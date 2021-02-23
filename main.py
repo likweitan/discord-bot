272a98bf7e5e7795df9dcd93dfa5bd99d31aeb5b
Lik Wei Tan <likweitan@gmail.com>
2021-02-23 00:11:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+from keep_alive import keep_alive
+
+import discord
+import os
+
+import requests
+
+from discord.ext import commands,tasks
+from dotenv import load_dotenv
+
+from itertools import cycle
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix=".")
+
+text_channel_list = []
+for server in bot.guilds:
+    print(server)
+    for channel in server.channels:
+        print(channel)
+        if channel.type == 'Text':
+            text_channel_list.append(channel)
+
+def get_cases(country):
+    res = requests.get("https://coronavirus-19-api.herokuapp.com/countries/" + country)
+
+    print(res.status_code)
+
+    json = res.json()
+    print(json)
+    x = (json['todayCases'])
+    print('Total number of cases: ' + str(x))
+    return x
+
+@bot.event
+async def on_ready():
+  change_status.start()
+  #send_message.start(bot.guilds)
+  print('Bot is ready')
+  print(bot.guilds)
+
+@bot.command(
+  help="Uses come crazy logic to determine if pong is actually the correct value or not.",
+	brief="Prints pong back to the channel."
+  )
+async def ping(ctx):
+	await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
+
+
+@bot.command()
+async def tracking(ctx, *args):
+	response = ""
+
+	for arg in args:
+		response = response + " " + arg
+
+	await ctx.channel.send(response)
+
+@bot.event
+async def on_message(message):
+	if message.content.startswith('http'):
+		await message.channel.send("noob")
+
+	await bot.process_commands(message)
+
+@bot.command()
+async def corona(ctx, *args):
+    total_new_cases = get_cases(args[0])
+    embed=discord.Embed(title="sd")
+    embed.add_field(name="undefined", value="Total new cases today: " + str(total_new_cases), inline=False)
+    await ctx.send(embed=embed)
+
+@tasks.loop(seconds=60)
+async def change_status():
+  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers"))
+"""
+@tasks.loop(seconds=60)
+async def send_message(guild):
+  await guild.text_channels[0].send("I have joined the server")
+"""
+
+
+
+
+keep_alive()
+bot.run(TOKEN)
