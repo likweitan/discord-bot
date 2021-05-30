2f2a298efc71f484198411f710e6db8a6d99b8e4
Lik Wei <likweitan@gmail.com>
2021-05-30 20:43:21 +0800

    Saved

--- main.py
+++ main.py
@@ -6,23 +6,11 @@ import os
 import requests
 
 from discord.ext import commands,tasks
-from dotenv import load_dotenv
 
 
-load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-CONFIG = {
-  "apiKey": os.getenv('apiKey'),
-  "authDomain": os.getenv('authDomain'),
-  "databaseURL": os.getenv('databaseURL'),
-  "projectId": os.getenv('projectId'),
-  "storageBucket": os.getenv('storageBucket'),
-  "messagingSenderId": os.getenv('messagingSenderId'),
-  "appId": os.getenv('appId'),
-  "measurementId": os.getenv('measurementId'),
-}
 
 bot = commands.Bot(command_prefix=".")
 
